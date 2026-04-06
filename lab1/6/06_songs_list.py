7bbd99d6dbedb56cb1368af238e2842695aac7ae
redbullenjoyer <annanikulina06@mail.ru>
2026-04-06 10:49:01 +0500

    лаб1

--- lab1/6/06_songs_list.py
+++ lab1/6/06_songs_list.py
@@ -23,7 +23,19 @@ violator_songs_list = [
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
+# Находим время звучания каждой песни из списка
+halo_time = violator_songs_list[3][1]  # 'Halo' под индексом 3
+enjoy_time = violator_songs_list[5][1]  # 'Enjoy the Silence' под индексом 5
+clean_time = violator_songs_list[8][1]  # 'Clean' под индексом 8
+
+# Суммируем время
+total_time_1 = halo_time + enjoy_time + clean_time
+
+# Округляем результат до 2 знаков после запятой
+total_time_rounded_1 = round(total_time_1, 2)
+
+# Выводим результат
+print(f'Три песни звучат {total_time_rounded_1} минут')
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -41,4 +53,16 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+# Находим время звучания каждой песни из словаря
+sweetest_time = violator_songs_dict['Sweetest Perfection']
+policy_time = violator_songs_dict['Policy of Truth']
+blue_dress_time = violator_songs_dict['Blue Dress']
+
+# Суммируем время
+total_time_2 = sweetest_time + policy_time + blue_dress_time
+
+# Округляем результат до 2 знаков после запятой
+total_time_rounded_2 = round(total_time_2, 2)
+
+# Выводим результат
+print(f'А другие три песни звучат {total_time_rounded_2} минут')
